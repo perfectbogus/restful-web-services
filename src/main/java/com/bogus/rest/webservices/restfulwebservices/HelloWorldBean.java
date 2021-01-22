c3a99c59011af7d0e1d525c764a7ada563a074c9
eduardo escobar <war_army_10@hotmail.com>
2021-01-22 14:04:55 -0600

    Step:4 - enhace helloWorld into a bean

---
+++ src/main/java/com/bogus/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.bogus.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+    private String meeting;
+
+    public HelloWorldBean(String str) {
+        this.meeting = str;
+    }
+
+    public String getMeeting() {
+        return meeting;
+    }
+
+    public void setMeeting(String meeting) {
+        this.meeting = meeting;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "meeting='" + meeting + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/bogus/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/bogus/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.bogus.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World Bean");
     }
 
 }
