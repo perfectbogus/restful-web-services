eff854c4faec91fed9efd1a0731aa922d49e6e9d
eduardo escobar <war_army_10@hotmail.com>
2021-01-22 22:03:07 -0600

    Step 8: Working with user resource

---
+++ src/main/java/com/bogus/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,26 @@
+package com.bogus.rest.webservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserDaoService service;
+
+    //retrieveAllUsers
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+    //retrieveUser
+    @GetMapping("/user/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
+    }
+}
