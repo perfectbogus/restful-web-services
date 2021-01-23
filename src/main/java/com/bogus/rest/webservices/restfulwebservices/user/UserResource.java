049dc0b1eaaa90cd75816d2fcb58b39231e917c0
eduardo escobar <war_army_10@hotmail.com>
2021-01-22 22:44:28 -0600

    Step 9: Creating a get and post request using postman

--- src/main/java/com/bogus/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/bogus/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,9 +1,7 @@
 package com.bogus.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +21,13 @@ public class UserResource {
     public User retrieveUser(@PathVariable int id){
         return service.findOne(id);
     }
+
+    // Create user
+    // input - details of user
+    // output - created & return the created URI
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+    }
+
 }
