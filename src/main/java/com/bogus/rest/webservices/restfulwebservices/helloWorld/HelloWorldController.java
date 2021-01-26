5cefc58073040fe5e67401e0640ceb29b1bf9d86
eduardo escobar <war_army_10@hotmail.com>
2021-01-25 20:09:23 -0600

    Step 18: Simplify Internationalization

--- src/main/java/com/bogus/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
+++ src/main/java/com/bogus/rest/webservices/restfulwebservices/helloWorld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.bogus.rest.webservices.restfulwebservices.helloWorld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -20,23 +21,22 @@ public class HelloWorldController {
     private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
     }
 
     @GetMapping(path = "/hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World Bean");
     }
 
     @GetMapping(path = "/hello-world/path-variable/{name}")
-    public HelloWorldBean helloWorldBean(@PathVariable String name){
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
     @GetMapping(path = "/hello-world-internationalized")
-    public String helloWorldInternationalized(
-            @RequestHeader(name = "Accept-Language", required = false) Locale locale){
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
